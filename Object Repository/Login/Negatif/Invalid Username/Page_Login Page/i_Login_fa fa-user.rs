8844fa87ec854f7bee2d218743f4dd9ab21868e9
Awanprasetya <kurniawan.saepudin21@gmail.com>
2025-07-14 11:00:39 +0700

    Add additional negatif login test : username and password empty

---
+++ Object Repository/Login/Negatif/Invalid Username/Page_Login Page/i_Login_fa fa-user.rs
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_Login_fa fa-user</name>
+   <tag></tag>
+   <elementGuidId>43be25cd-a877-4e6c-809f-031d3eaea197</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>i.fa.fa-user</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//i</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>i >> nth=0</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+      <webElementGuid>43e21872-14fc-4f42-a50e-b18cd48fcc29</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fa fa-user</value>
+      <webElementGuid>d47b8bae-933c-4dc6-80be-425ec3d8c51a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;login-container&quot;]/form[1]/div[@class=&quot;form-group input-icon&quot;]/i[@class=&quot;fa fa-user&quot;]</value>
+      <webElementGuid>6b26cecb-cfc6-4d38-b736-18d6d6091752</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//i</value>
+      <webElementGuid>f392ed20-77ea-434e-817d-2ff5cc43f8bb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
